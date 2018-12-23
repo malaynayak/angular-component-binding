6382ac24ff33b5f9cbe35cef23fdd0f574cdba8b
Malay Nayak <malay.nayak@techaspect.com>
2018-12-23 16:49:09 +0530

    Component lifecycle.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  Input,
+  ViewEncapsulation,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,14 +20,61 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   // View encapsulation
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
 
   // Decorator to expose element to another component(with Alias).
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() {
+    console.log("Constructor Called");
+  }
+
+  // Called after a bound input property changes.
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngOnChanges Called");
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log("ngOnInit Called");
+  }
+
+  // Called during every change detection run.
+  ngDoCheck() {
+    console.log("ngDoCheck Called");
+  }
+
+  // Called after content (ng-content) has been projected into view.
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit Called");
+  }
+
+  // Called every time the projected content has been checked.
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked Called");
+  }
+
+  // Called after the component’s view (and child views) has been initialized.
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit Called");
+  }
+
+  // Called every time the view (and child views) have been checked.
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked Called");
+  }
+
+  // Called once the component is about to be destroyed.
+  ngOnDestroy() {
+    console.log("ngOnDestroy Called");
   }
 
 }
